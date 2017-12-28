cfc9099260693fbdf193b67bb787526f6682d0ac
chanyi <chanyipk@naver.com>
2017-12-28 22:53:28 +0900

    modify Programmers_Q2 code

--- Programmers-Q2/Parkchanyi.java
+++ Programmers-Q2/Parkchanyi.java
@@ -1,4 +1,28 @@
 
-public class Parkchanyi {
+class SumMatrix {
+	int[][] sumMatrix(int[][] A, int[][] B) {
+		int x = A.length;
+  		int y = A[0].length;
+    		int[][] answer = new int[x][y];
+    		for(int i = 0; i < x; i++){
+    			for(int j = 0; j < y; j++){
+      				answer[i][j] = A[i][j] + B[i][j];
+      			}
+    		}
+		return answer;
+	}
 
+	// 아래는 테스트로 출력해 보기 위한 코드입니다.
+	public static void main(String[] args) {
+		SumMatrix c = new SumMatrix();
+		int[][] A = { { 1, 2 }, { 2, 3 } };
+		int[][] B = { { 3, 4 }, { 5, 6 } };
+		int[][] answer = c.sumMatrix(A, B);
+		if (answer[0][0] == 4 && answer[0][1] == 6 && 
+				answer[1][0] == 7 && answer[1][1] == 9) {
+			System.out.println("맞았습니다. 제출을 눌러 보세요");
+		} else {
+			System.out.println("틀렸습니다. 수정하는게 좋겠어요");
+		}
+	}
 }

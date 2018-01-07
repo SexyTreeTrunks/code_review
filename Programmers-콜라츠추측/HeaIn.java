6baa7066c1f4c045a1dc395f8ea63a046a2b1a66
HeaIn <sarahjj.0809@gmail.com>
2018-01-07 17:16:46 +0900

    콜라츠추측 수정

--- Programmers-콜라츠추측/HeaIn.java
+++ Programmers-콜라츠추측/HeaIn.java
@@ -1,29 +1,22 @@
-class Caesar {
-    String caesar(String s, int n) {
-        StringBuilder result = new StringBuilder();
-        for(int i = 0; i < s.length(); i++) {
-            char temp = s.charAt(i);
-            if(temp == ' ') {
-                result.append(' ');
-                continue;
-            }
-
-            if(temp >= 'a' && temp <='z') {
-                temp += n;
-                while(!(temp >= 'a' && temp <= 'z'))
-                    temp -= 26;
-            } else {
-                temp += n;
-                while(!(temp >= 'A' && temp <= 'Z'))
-                    temp -= 26;
-            }
-            result.append(temp);
-        }
-        return result.toString();
+class Collatz {
+	public int collatz(long num) {
+		int answer = 0;
+		while(num != 1) {
+      if(answer >= 500)
+        return -1;
+      
+      if(num % 2 == 0)
+        num /= 2;
+      else
+        num = num * 3 + 1;
+      answer++;
     }
+		return answer;
+	}
 
-    public static void main(String[] args) {
-        Caesar c = new Caesar();
-        System.out.println("s는 'a B z', n은 4인 경우: " + c.caesar("a B z", 4));
-    }
+	public static void main(String[] args) {
+		Collatz c = new Collatz();
+		int ex = 6;
+		System.out.println(c.collatz(ex));
+	}
 }

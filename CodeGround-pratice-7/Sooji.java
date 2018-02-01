f510389b2bb1884f1889ed89b24f83cfe150e708
SoojiNoh <thensj6873@gmail.com>
2018-02-01 15:32:31 +0900

    complete submit

--- CodeGround-pratice-7/Sooji.java
+++ CodeGround-pratice-7/Sooji.java
@@ -1,6 +1,3 @@
-package codeground;
-
-import java.util.*;
 /*
 You should use the statndard input/output
 
@@ -10,11 +7,15 @@ Do not use file input and output
 
 Please be very careful. 
 */
+
+// import java.util.Scanner;
+import java.util.*;
+
 /*
    As the name of the class should be Solution , using Solution.java as the filename is recommended.
    In any case, you can execute your program by running 'java Solution' command.
  */
-class GoodNumber {
+class Solution {
 	static int Answer;
 
 	public static void main(String args[]) throws Exception	{
@@ -31,10 +32,11 @@ class GoodNumber {
 		 */
 		Scanner sc = new Scanner(System.in);
 		//Scanner sc = new Scanner(new FileInputStream("input.txt"));
+
 		int T = sc.nextInt();
 		for(int test_case = 0; test_case < T; test_case++) {
 
-			// Answer = 0;
+			Answer = 0;
 			/////////////////////////////////////////////////////////////////////////////////////////////
 			int size = sc.nextInt();
 			
@@ -43,24 +45,22 @@ class GoodNumber {
 				input[i] = sc.nextInt();
 			}
 			
-			boolean[] composedNum = new boolean[40001]; //composedNum는 배열 내 존재하는 앞에 존재하는 두 수의 합으로 이루어진 숫자들의 존재여부 flag
-//			Arrays.sort(input);
-			
-		System.out.println(Arrays.toString(input));
-//			System.out.println("array["+i+"]:"+input[i]);		
+			boolean[] composedNum = new boolean[400001];
+
 		
-			for(int num=0 ; num<input.length-1; num++){
+			for(int num=0 ; num<size; num++){
 				boolean found=false;
 				for(int elem=0 ; elem<num&&!found; elem++){
-					if(composedNum[input[num]-input[elem]+20000]==true) //만약 좋은숫자후보(num)가 그 이전 숫자(elem)와의 차가 배열 composedNum 존재(True)한다면 좋은 숫자가 맞다.
+					if(composedNum[input[num]-input[elem]+200000]==true) 
 						found=true;
 				}
 				for(int elem=0 ; elem<=num ; elem++)
-					composedNum[input[num]+input[elem]+20000]=true; //주어진 index의 새로운 좋은숫후보로 composedNum를 업데이트 한다.
-				if(found==true) {
+					composedNum[input[num]+input[elem]+200000]=true;
+				if(found) {
 					Answer++;
 				}
 			}
+
 			/////////////////////////////////////////////////////////////////////////////////////////////
 
 
